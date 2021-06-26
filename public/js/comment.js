5c670fb0edd2ca650f7d9561546697e35a380f18
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-26 16:00:38 -0400

    working on fixing the errors on 127.0.0.1:3001

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,24 +1,30 @@
 const commentFormHandler = async (event) => {
+  try {
     event.preventDefault();
-
     // Collect values from the login form
     const content = document.querySelector('#commentinput').value.trim();
-
-    if (content) {
+    
+  
+    if (content && event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/comment', {
+      const response = await fetch(`/api/comments/${id}`, {
         method: 'POST',
         body: JSON.stringify({ content }),
         headers: { 'Content-Type': 'application/json' },
       });
-
+  
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/post');
+        document.location.replace(`/post/${id}`);
       } else {
         alert(response.statusText);
       }
     }
+
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
   };
 
 document
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,44 @@
 const logout = async () => {
+  try {
     const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
-  
+
     if (response.ok) {
       document.location.replace('/');
     } else {
       alert(response.statusText);
     }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+var idleTime = 0;
+var idleInterval = setInterval(timerIncrement, 60000);
+
+document.addEventListener('mousemove', function() {
+  idleTime = 0;
+})
+      
+document.addEventListener('keydown', function() {
+  idleTime = 0;
+});
+
+document.addEventListener('keyup', function() {
+  idleTime = 0;
+});
+
+//The user is logged out if their screen is idle for 5 minutes
+function timerIncrement() {
+  idleTime = idleTime + 1;
+  if (idleTime > 5) {
+    logout();
+  }
+}
+
+document
+  .querySelector('#logout')
+  .addEventListener('click', logout);
