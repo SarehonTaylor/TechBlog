ab31a2109fc693ce0dee3ae24f9ee4b9e9f2e00c
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-26 09:23:35 -0400

    fixing typos where data says project and not post

--- models/index.js
+++ models/index.js
@@ -7,26 +7,26 @@ User.hasMany(Post, {
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
+
 Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE'
-  });
-  
-  Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-  });
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
 
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
-  User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
-  
-  Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
 module.exports = { User, Post, Comment };
