d69d6e388f9eff697d749803cf72ba7f41e6b872
Sarehon Taylor <sarehontaylor@icloud.com>
2021-06-27 00:44:33 -0400

    fixed typo in comment.js that said comments for api route

--- public/js/comment.js
+++ public/js/comment.js
@@ -8,7 +8,7 @@ const commentFormHandler = async (event) => {
     if (content && event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
       // Send a POST request to the API endpoint
-      const response = await fetch(`/api/comments/${id}`, {
+      const response = await fetch(`/api/comment/${id}`, {
         method: 'POST',
         body: JSON.stringify({ content }),
         headers: { 'Content-Type': 'application/json' },
